f2e26e04a513753414afd35668b12ff8062bf008
mels_gbkim <melsgbkim@gmail.com>
2018-07-08 23:54:52 +0900

    MoveFlag.cs 임시완성

---
+++ Assets/Script/MoveFlag/MoveFlag.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveFlag : MonoBehaviour
+{
+    public MeshRenderer Line;
+    public Transform Flag;
+
+    public Vector3 FromPos;
+
+    public float LineSpeed;
+    float LineProgress;
+
+    public float FloatingTime;
+    public float FloatingLength;
+    float FloatingProgressTime = 0f;
+    // Use this for initialization
+    void Start()
+    {
+        //Line.material.mainTextureOffset = new Vector2(10f, 1f);
+        SetDest(new Vector3(0, 0, 0), new Vector3(3, 0, 0));
+
+        /*Line.material.mainTextureScale = new Vector2(3f, 1f);
+        Line.material.mainTextureOffset = new Vector2(0.5f, 0f);*/
+    }
+
+    void SetDest(Vector3 from, Vector3 dest)
+    {
+        FromPos = from;
+        transform.localPosition = dest;
+
+        Vector3 vec = (from - dest);
+        Line.material.mainTextureScale = new Vector2(vec.magnitude, 1f);
+        Line.transform.localPosition = vec * 0.5f;
+        Line.transform.localScale = new Vector3(vec.magnitude, Line.transform.localScale.y, Line.transform.localScale.z);
+        Line.transform.localRotation = Quaternion.FromToRotation(vec, from) * Quaternion.Euler(90,0,0);
+        //vec.normalized
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FloatingProgressTime += Time.deltaTime;
+        if (FloatingProgressTime > FloatingTime)
+            FloatingProgressTime -= FloatingTime;
+        Flag.localPosition = new Vector3(Flag.localPosition.x,(Mathf.Sin(FloatingProgressTime / FloatingTime * 2 * Mathf.PI) + 1) * 0.5f * FloatingLength, Flag.localPosition.z);
+
+        //        Line.material.mainTexture.wrapMode = TextureWrapMode.Repeat;
+        LineProgress += Time.deltaTime * LineSpeed;
+        if (LineProgress > 1f)
+            LineProgress -= 1f;
+        Line.material.mainTextureOffset = new Vector2(LineProgress, 0f);
+    }
+}
